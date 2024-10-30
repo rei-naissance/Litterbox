861f18b6321596bdfbc7001499cd4f560ed8e40f
Rei <philippeandreid@gmail.com>
2024-10-31 06:46:38 +0800

    stash changes

--- Litterbox/settings.py
+++ Litterbox/settings.py
@@ -136,6 +136,9 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.Student'
---
+++ dashboard/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Post, Comment, Report
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'image'] # add image to fields when ready to implement
+        widgets = {
+            'content': forms.TextInput(attrs={'class': 'w-full focus:outline-none rounded-lg border-none font-normal text-gray-font2 bg-gray-box text-sm placeholder-gray-font', 'placeholder': 'What\'s on your mind?'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+class ReportForm(forms.ModelForm):
+    class Meta:
+        model = Report 
+        fields = ['reason', 'reason_category']
+        widgets = {
+            'reason': forms.Textarea(attrs={'placeholder': 'Please provide a reason for reporting this post'}),
+            'reason_category': forms.Select(attrs={'class': 'form-select'}),
+        }
